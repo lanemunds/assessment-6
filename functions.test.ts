6540ccf3aa5ffe4cf6f9a9190eca31160288a968
lanemunds <lanemunds@gmail.com>
2022-09-23 14:59:19 -0600

    fifthCommit

--- functions.test.ts
+++ functions.test.ts
@@ -1,11 +1,12 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    test('The first name of the test data should be Patten', () => {
-        expect(Array).toBeTruthy()})
+    test('test that that it returns something', () => {
+        expect(shuffleArray([1,2,3,4,5])).toBeTruthy()})
    
-    test('The first name of the test data should be Patten', () => {
-        expect(Array).toBeTruthy()})
+    test('The function is returning an array of the correct length', () => {
+        shuffleArray([1,2,3,4,5])
+        expect(shuffleArray.length).toBe(5)})
 
 
     })
